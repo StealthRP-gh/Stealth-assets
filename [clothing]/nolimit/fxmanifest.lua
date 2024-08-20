2fc7ac7435c1fa416a3974d74707a8f4af8cb4d8
Stealth-VPS <coreykemn@gmail.com>
2024-08-20 13:22:56 +0100

    noliumit

---
+++ [clothing]/nolimit/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_nolimit.meta',
+  'mp_f_freemode_01_mp_f_nolimit.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_nolimit.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_nolimit.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
