71c95ef5230f863e693576dd2a256ea39ed41e22
Stealth-VPS <coreykemn@gmail.com>
2024-08-20 14:00:26 +0100

    cehivle

---
+++ [vehicles]/mc9-vehiclepack/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version "cerulean"
+game "gta5"
+
+author "Noxius"
+description "A Mass addon vehicle pack"
+version "1.0.0"
+
+data_file "VEHICLE_LAYOUTS_FILE"    "stream/**/data/vehiclelayouts.meta"
+data_file "CARCOLS_FILE"            "stream/**/data/carcols.meta"
+data_file "HANDLING_FILE"           "stream/**/data/handling.meta"
+data_file "VEHICLE_METADATA_FILE"   "stream/**/data/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE"  "stream/**/data/carvariations.meta"
+
+
+files {
+    "stream/**/data/vehiclelayouts.meta",
+    "stream/**/data/carcols.meta",
+    "stream/**/data/handling.meta",
+    "stream/**/data/vehicles.meta",
+    "stream/**/data/carvariations.meta",
+}
